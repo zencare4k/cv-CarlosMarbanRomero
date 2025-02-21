0d981329bfd3c704ebe0851e886fff193b85af65
Carlos Marban <152007178+zencare4k@users.noreply.github.com>
2025-02-21 23:35:17 +0100

    xd

--- scripts.js
+++ scripts.js
@@ -6,4 +6,21 @@ function toggleMenu() {
 function toggleDarkMode() {
     const body = document.body;
     body.classList.toggle('dark-mode');
+    const darkModeToggle = document.querySelector('.dark-mode-toggle');
+    if (body.classList.contains('dark-mode')) {
+        darkModeToggle.textContent = '🌞'; // Sol para modo oscuro
+    } else {
+        darkModeToggle.textContent = '🌜'; // Luna para modo claro
+    }
 }
+
+// Inicializa el icono del botón de modo oscuro al cargar la página
+document.addEventListener('DOMContentLoaded', () => {
+    const body = document.body;
+    const darkModeToggle = document.querySelector('.dark-mode-toggle');
+    if (body.classList.contains('dark-mode')) {
+        darkModeToggle.textContent = '🌞'; // Sol para modo oscuro
+    } else {
+        darkModeToggle.textContent = '🌜'; // Luna para modo claro
+    }
+});
